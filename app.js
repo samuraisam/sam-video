44a8ff1329ac37d4c468cbde009ce3d4c11e0f2d
Samuel Sutch <sam@sutch.net>
2016-11-24 20:52:33 -0800

    form and mobile styles

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/form', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var apiKey = process.env.MAILGUN_KEY;
+var domain = process.env.MAILGUN_DOMAIN;
+var mailgun = require("mailgun-js")({apiKey: apiKey, domain: domain});
+var siteKey = process.env.RECAPTCHA_KEY;
+var secret = process.env.RECAPTCHA_SECRET;
+var recaptcha2 = require('recaptcha2')
+var recaptcha = new recaptcha2({siteKey: siteKey, secretKey: secret});
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  var data = {
+    from: 'SAM VIDEO <sam+video@'+domain+'>',
+    to: req.body.email,
+    subject: 'new contact from ' + req.body.name + ' at ' + req.body.company,
+    text: 'product:\n' + req.body.product + '\nwhyus:\n' + req.body.whyus
+  };
+  console.log('data', data);
+  console.log('recaptcha', req.body['g-recaptcha-response']);
+  try {
+    recaptcha.validate(req.body['g-recaptcha-response']).then(function() {
+      mailgun.messages().send(data, function(err,bod) {
+        console.log('mailgun err:', err, 'mailgun body:', bod);
+      });
+    }).catch(function() {
+      console.log("recaptcha error:", arguments);
+    });
+  } catch(e) {
+    console.error(e);
+  }
 });
 
 module.exports = router;
